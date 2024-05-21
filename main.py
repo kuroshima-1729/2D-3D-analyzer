0aa84e8b0de3f36369debee690ae3bd15c21c4e0
kuroshima <Leonardo-V@outlook.jp>
2024-05-21 21:40:26 +0900

    add resize process to main.py

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("image_processor")
+import argparse
+import cv2
 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_image_path', type=str, default=None)
+    parser.add_argument('--process_type', choices=['resize', 'no_process'], type=str, default=None)
+    parser.add_argument('--resize_width', type=int, default=None)
+    parser.add_argument('--resize_height', type=int, default=None)
+    parser.add_argument('--save_image_path', type=str, default=None)
+    args = parser.parse_args()
+
+    image_data = cv2.imread(args.input_image_path)
+    if args.process_type=='resize':
+        image_data = cv2.resize(image_data, (args.resize_width, args.resize_height))
+    elif args.process_type=='no_process':
+        pass
+
+    cv2.imwrite(args.save_image_path, image_data)
+    
 
 if __name__ == "__main__":
     main()
