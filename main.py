a291de78bfd6eac4728a847c7f1b9d65c43a317d
kuroshima <Leonardo-V@outlook.jp>
2024-05-23 20:55:56 +0900

    handling multiple input image

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import cv2
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_image_path', type=str, default=None)
+    parser.add_argument('--input_image_paths', nargs='+', type=str, default=None)
     parser.add_argument('--process_type', choices=['resize', 'no_process'], type=str, default=None)
+
+    # resize parameter
     parser.add_argument('--resize_width', type=int, default=None)
     parser.add_argument('--resize_height', type=int, default=None)
+
     parser.add_argument('--save_image_path', type=str, default=None)
     args = parser.parse_args()
 
-    image_data = cv2.imread(args.input_image_path)
+    image_data = cv2.imread(args.input_image_paths[0])
     if args.process_type=='resize':
         image_data = cv2.resize(image_data, (args.resize_width, args.resize_height))
     elif args.process_type=='no_process':
